40511cf6c12478f2d040966343cdba51744c41fa
vijeth114055 <152253673+vijeth114055@users.noreply.github.com>
2023-11-28 12:10:43 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,4 +5,6 @@ streamlit.header("Breakfast Menu")
 streamlit.text("🥣Bread omlette🍞 and Paneer sandwich")
 streamlit.text("🥗Plain dosa and Sambar with 🥑flavoured rice")
 streamlit.text("🐔Hard boiled eggs")
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
    
