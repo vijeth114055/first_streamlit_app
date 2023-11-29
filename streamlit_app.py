6cb56913373e249dc4eb0a4e03895c28895aeb2e
vijeth114055 <152253673+vijeth114055@users.noreply.github.com>
2023-11-29 14:55:03 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 
 import streamlit
-#import pandas
+import pandas
 streamlit.title("My parents breakfast menu")
 streamlit.header("Breakfast Menu")
 streamlit.text("🥣Bread omlette🍞 and Paneer sandwich")
@@ -17,7 +17,7 @@ streamlit.dataframe(fruits_to_show)
 
 
 streamlit.header("Fruityvice Fruit!!!!!!")
-#import requests
+import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
